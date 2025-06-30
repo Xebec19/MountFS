b08dcba8b39cc4d8c7aa453eb7469b444a7f1ea6
xebec19 <rohandeveloper106@gmail.com>
2025-06-30 11:41:41 +0530

    feat: Add Remote type with file operations and context support

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -1,10 +1,22 @@
 package fs
 
-import "time"
+import (
+	"context"
+	"io"
+	"time"
+)
 
 type FileInfo struct {
 	Name     string
 	Size     int64
 	ModeTime time.Time
 	IsDir    bool
+}
+
+type Remote struct {
+	List   func(ctx context.Context, path string) ([]FileInfo, error)
+	Get    func(ctx context.Context, path string) (io.ReadCloser, error)
+	Put    func(ctx context.Context, path string, data io.Reader) error
+	Delete func(ctx context.Context, path string) error
+	Stat   func(ctx context.Context, path string) (FileInfo, error)
 }
